6d984125113653c047a5f041962188b9ca5b5744
Tyler Noble <tylersnoble@gmail.com>
2020-06-29 01:09:04 -0400

    Made it so testing library now runs
    took way too long to figure out.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {FirebaseAppProvider} from 'reactfire';
+import firebaseConfig from './config/config'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+  <App />
+  </FirebaseAppProvider>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
