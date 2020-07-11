2e8660e6bd4911a04958255494de132105c29bea
Tyler Noble <tylersnoble@gmail.com>
2020-07-11 17:00:55 -0400

    Changed Folder Structure so ESLint works now

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
-app.get('/',(req,res) => {
-  res.send("Hey the backend is running by the way")
-})
+app.get('/', (req, res) => {
+  res.send('Hey the backend is running by the way');
+});
